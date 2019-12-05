719c09cef5047c5c2aa5b39271ebe320cabb068c
ASterniewski <drapiszon@gmailcom>
2019-12-05 15:04:27 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { MatMenuModule, MatButtonModule, MatIconModule, MatCardModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule }   from '@angular/forms';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { NotificationsAddComponent } from './notifications-add/notifications-add.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    NotificationsComponent,
+    NotificationsAddComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/notifications-add/notifications-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-add',
+  templateUrl: './notifications-add.component.html',
+  styleUrls: ['./notifications-add.component.css']
+})
+export class NotificationsAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
